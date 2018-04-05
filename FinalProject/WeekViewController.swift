ef85327ec192dd6c28fce5ea9ba11ec15a7d7b33
Jonathan Yee <jy6vd@mail.missouri.edu>
2018-04-05 12:31:48 -0500

    Got the week to show up

---
+++ FinalProject/WeekViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WeekViewController.swift
+//  FinalProject
+//
+//  Created by Jonathan Yee on 4/5/18.
+//  Copyright © 2018 Jonathan Yee. All rights reserved.
+//
+
+import UIKit
+
+class WeekViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalProject/WeeksViewController.swift
@@ -0,0 +1,58 @@
+//
+//  WeeksViewController.swift
+//  FinalProject
+//
+//  Created by Jonathan Yee on 4/5/18.
+//  Copyright © 2018 Jonathan Yee. All rights reserved.
+//
+
+import UIKit
+
+class WeeksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var days: [String] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    @IBOutlet weak var tableview: UITableView!
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return days.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath) as! WeekTableViewCell
+        cell.days.text = days[indexPath.row];
+        
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableview.dataSource = self
+        tableview.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
