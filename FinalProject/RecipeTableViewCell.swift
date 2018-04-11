60bc91d89d13272513cd99ac7bf8c4ce681f4161
Jonathan Yee <jy6vd@mail.missouri.edu>
2018-04-10 23:26:00 -0500

    Able to display recipe

---
+++ FinalProject/RecipeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeTableViewCell.swift
+//  FinalProject
+//
+//  Created by Jonathan Yee on 4/10/18.
+//  Copyright © 2018 Jonathan Yee. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var foodTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FinalProject/RecipeViewController.swift
@@ -0,0 +1,55 @@
+//
+//  RecipeViewController.swift
+//  FinalProject
+//
+//  Created by Jonathan Yee on 4/10/18.
+//  Copyright © 2018 Jonathan Yee. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
+    @IBOutlet weak var recipeTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        recipeTableView.dataSource = self
+        recipeTableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath) as! RecipeTableViewCell
+        cell.foodTitle.text = "Donut"
+        cell.foodImage.image = UIImage(named: "donut")
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
