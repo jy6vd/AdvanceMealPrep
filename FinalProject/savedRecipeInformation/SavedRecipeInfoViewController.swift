c5a4cb5d0b90c805ef9fb8eaa5076ddb781ae3ee
Jonathan Yee <jy6vd@mail.missouri.edu>
2018-04-19 13:42:01 -0500

    need to finish saved recipe ingredient info

---
+++ FinalProject/savedRecipeInformation/SavedRecipeInfoViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SavedRecipeInfoViewController.swift
+//  FinalProject
+//
+//  Created by Jonathan Yee on 4/19/18.
+//  Copyright © 2018 Jonathan Yee. All rights reserved.
+//
+
+import UIKit
+
+class SavedRecipeInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //var volume_units: [String] = ["tsp", "tbs", "fl oz", "cup", "pint", "quart", "gallon", "ml", "l"]
+    var ingredients: [String] = ["Onion", "Apple Juice", "Salt", "Pepper", "Olive oil"]
+    var servingsize: [String] = ["1 fl oz","2 tbs","3 cups","4 tsp","5 pint"];
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredients.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ingredientCell", for: indexPath) as! SavedRecipeInfoTableViewCell
+        cell.ingredientName.text = ingredients[indexPath.row]
+        cell.servingSize.text = servingsize[indexPath.row]
+        
+        return cell
+    }
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var tabBar: UISegmentedControl!
+    @IBOutlet weak var servingSizeLabel: UILabel!
+    @IBOutlet weak var ingredientTableView: UITableView!
+    @IBOutlet weak var removeButton: UIBarButtonItem!
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ingredientTableView.dataSource = self
+        ingredientTableView.delegate = self
+        
+        foodImage.image = UIImage(named: "donut")
+        servingSizeLabel.text = "Serving size: 1"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
