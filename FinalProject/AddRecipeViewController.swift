565b84159979ff3264703435d14f93e3f076fbce
Hayden <Hayden@mwc-056066.dhcp.missouri.edu>
2018-04-24 13:43:13 -0500

    Added segmented control function for add recipe view

--- FinalProject/AddRecipeViewController.swift
+++ FinalProject/AddRecipeViewController.swift
@@ -10,19 +10,45 @@ import UIKit
 class AddRecipeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
     var ingredients: [String] = ["Onion", "Apple Juice", "Salt", "Pepper", "Olive oil"]
-    var servingsize: [String] = ["1 fl oz","2 tbs","3 cups","4 tsp","5 pint"];
+    var servingsize: [String] = ["1 fl oz","2 tbs","3 cups","4 tsp","5 pint"]
+    var directions: [String] = [
+        "Heat up pan",
+        "Pour olive oil in",
+        "Chopped onion",
+        "Add salt and pepper",
+        "Cook for 15 mins"
+    ]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ingredients.count
+        var count = 0
+        switch(tabBar.selectedSegmentIndex){
+        case 0:
+            count = ingredients.count
+        case 1:
+            count = directions.count
+        default:
+            break
+        }
+        return count
     }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientDisplayTableViewCell", for: indexPath) as! IngredientDisplayTableViewCell
-        cell.ingredientName.text = ingredients[indexPath.row]
-        cell.servingSize.text = servingsize[indexPath.row]
-
+        switch(tabBar.selectedSegmentIndex){
+        case 0:
+            cell.ingredientName.text = ingredients[indexPath.row]
+            foodTitle.title = ingredients[indexPath.row]
+            cell.servingSize.text = servingsize[indexPath.row]
+        case 1:
+            cell.ingredientName.text = directions[indexPath.row]
+            foodTitle.title = ingredients[indexPath.row]
+            cell.servingSize.text = " "
+        default:
+            break
+        }
         return cell
     }
     
@@ -30,7 +56,11 @@ class AddRecipeViewController: UIViewController, UITableViewDataSource, UITableV
     @IBOutlet weak var tabBar: UISegmentedControl!
     @IBOutlet weak var servingSizeLabel: UILabel!
     @IBOutlet weak var ingredientTableView: UITableView!
+    @IBOutlet weak var foodTitle: UINavigationItem!
     
+    @IBAction func segmentControlChanged(_ sender: UISegmentedControl) {
+        ingredientTableView.reloadData()
+    }
     
     
     
@@ -39,6 +69,9 @@ class AddRecipeViewController: UIViewController, UITableViewDataSource, UITableV
         super.viewDidLoad()
         ingredientTableView.dataSource = self
         ingredientTableView.delegate = self
+        
+        foodImage.image = UIImage(named: "donut")
+        servingSizeLabel.text = "Serving size: 1"
         // Do any additional setup after loading the view, typically from a nib.
     }
     
